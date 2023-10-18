a5640b201e9570d1874427dcb63b6f125b53ccff
TTHTech <tthoai2401.learn@gmail.com>
2023-10-18 13:10:08 +0700

    them silde product

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const adressbtn = document.querySelector('#adress-form')
 const adresscl = document.querySelector('#adress-close')
 
-
 adressbtn.addEventListener("click", function(){
     document.querySelector('.adress-form').style.display = "flex";
 })
@@ -13,6 +12,48 @@ adresscl.addEventListener("click", function(){
 
 // slider==================================================
 const rightbtn = document.querySelector('.ti-angle-right')
+const leftbtn = document.querySelector('.ti-angle-left')
+const imgNumber = document.querySelectorAll('.slider-content-left-top img')
+let index = 0
 rightbtn.addEventListener("click", function(){
-    document.querySelector(".slider-content-left-top").style.right = "100%"
+    index = index + 1
+    if(index > imgNumber.length-1){
+        index = 0
+    }
+    document.querySelector(".slider-content-left-top").style.right = index * 100+"%"
+
 })
+leftbtn.addEventListener("click", function(){
+    index = index - 1
+    if(index <= 0){
+        index = imgNumber.length-1
+    }
+    document.querySelector(".slider-content-left-top").style.right = index * 100+"%"
+
+})
+//slider1------------------------------------------------
+const imgNumberLi = document.querySelectorAll('.slider-content-left-bottom li')
+imgNumberLi.forEach(function(image,index){    
+    image.addEventListener("click", function(){
+        removeactive()
+        document.querySelector(".slider-content-left-top").style.right = index * 100+"%"  
+        image.classList.add("active")
+    })
+})
+function removeactive(){
+    let imgactive = document.querySelector('.active')
+    imgactive.classList.remove("active")
+}
+
+// slider2======================
+function imgAuto(){
+    index = index + 1
+    if(index > imgNumber.length-1){
+        index = 0
+    }
+    removeactive()
+    document.querySelector(".slider-content-left-top").style.right = index * 100+"%"
+    imgNumberLi[index].classList.add("active")
+    
+}
+setInterval(imgAuto,3000)
